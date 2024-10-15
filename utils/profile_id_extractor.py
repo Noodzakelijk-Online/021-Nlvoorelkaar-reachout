770e2e2376ca35c831059824620d8a9390f98932
noodzakelijkonline@gmail.com <noodzakelijkonline@gmail.com>
2024-10-15 19:29:51 +0300

    updated version with reminder service

--- utils/profile_id_extractor.py
+++ utils/profile_id_extractor.py
@@ -2,10 +2,10 @@ import time
 
 import requests
 from bs4 import BeautifulSoup
+from cffi.cffi_opcode import PRIM_FLOAT
 
 from config.settings import headers
 from models.sessionmanager import SessionManager
-
 
 def get_profile_id(offer_url):
     response = SessionManager.get_session().get(offer_url, headers=headers)
@@ -40,3 +40,28 @@ def get_profile_id(offer_url):
             print("No div with class 'block block--small block--square text--center first' found.")
     else:
         print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
+
+
+def get_offer_url_from_chat_page(chat_url):
+    response = SessionManager.get_session().get(chat_url, headers=headers)
+    print("Checking offer url from chat url", chat_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        offer_dd = soup.select_one('#content > div.site-retain.react-dashboard-menu > div > div.col-span-9 > dl > dd:nth-child(4) > a')
+        print(offer_dd)
+        if offer_dd:
+            if 'href' in offer_dd.attrs:
+                offer_url = offer_dd['href'].strip()
+
+                return f"https://www.nlvoorelkaar.nl{offer_url}"
+            else:
+                print("No <a> tag found or no href attribute.")
+                return None
+        else:
+            print("Offer <dd> element not found.")
+            return None
+    else:
+        print(f"Error: Received status code {response.status_code}")
+        return None
+
